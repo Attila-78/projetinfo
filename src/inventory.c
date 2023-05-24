61147d3490c6d1696a2127352efb1cb46b90609e
Bes4ce <123417833+Bes4ce@users.noreply.github.com>
2023-05-24 16:21:32 +0200

    Add files via upload

--- src/inventory.c
+++ src/inventory.c
@@ -1,23 +1,31 @@
 #include <inventory.h>
 //On crée une forntion pour ajouter des objects à l'inventaire
 void add_inventory(Case_inventory inventory[],Object_inventory item){
-    for(int i=3;i<=SIZE_INVENTORY;i++){
-        if(inventory[i].Objet==OBJECT_NULL|| inventory[i].amount<=16){
+    for(int i=2;i<=SIZE_INVENTORY;i++){
+        if(inventory[i].Objet==OBJECT_NULL){
             inventory[i].Objet=item;
             inventory[i].amount++;
+            return;
         }
-        else{
+        else if (inventory[i].Objet==item|| inventory[i].amount<SIZE_SLOT) 
+        {
+            inventory[i].amount++;
+        }
+        else
+        {
         break;
         }
     }
 }
 
-void add_points();
-
 //Fonction permettant de retirer des object
-void remove_inventory(){
-
-
+void remove_inventory(Case_inventory inventory[], int n ){
+    if (inventory[n].Objet-1<=0){
+        inventory[n].Objet=OBJECT_NULL;
+    }
+    else{
+        inventory[n].Objet--;
+    }
 
 }
 
